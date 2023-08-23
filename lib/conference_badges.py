dda582e635868dd43aa03db99d09b5ad611ba533
mary446 <135317707+mary446@users.noreply.github.com>
2023-08-23 07:38:20 +0300

    2 tests passing

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,19 @@
-def badge_maker(name):
-    return None
+def badge_maker(name = "Yna"):
+    return f"Hello, my name is {name}."
 
-def batch_badge_creator(names):
-    return None
+def batch_badge_creator(names = "Yna , Arel"):
+    return [f"Hello, my name is {name}." for name in names]
 
 def assign_rooms(names):
-    return None
+    rooms= range(1, 7)
+    assignment = []
+    for room in rooms:
+        assignment.append(f"Hello, {names[room - 1]}! You'll be assigned to room {room}!")
+    return assignment
 
 def printer(names):
-    return None
+    for badge in batch_badge_creator(names):
+        print(badge)
+    
+    for assignment in assign_rooms(names):
+        print (assignment)
